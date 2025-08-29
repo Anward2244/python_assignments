d4c22deb4722dc076f3915ce9258f0684183ef02
DUDEKULA ANWAR <danwar2805@gmail.com>
2025-08-29 12:38:13 +0530

    Add files via upload

--- 29Aug_Assignment.py
+++ 29Aug_Assignment.py
@@ -1,38 +1,48 @@
-# checking the given number is palindrome or not
+# # checking the given number is palindrome or not
 
-num1 = int(input('enter the number: '))
-temp = num1
-reverse_num = 0
+# num1 = int(input('enter the number: '))
+# temp = num1
+# reverse_num = 0
 
-while num1 > 0:
-    digit = num1 % 10
-    reverse_num = reverse_num * 10 + digit
-    num1 = num1 // 10
-print(reverse_num)
+# while num1 > 0:
+#     digit = num1 % 10
+#     reverse_num = reverse_num * 10 + digit
+#     num1 = num1 // 10
+# print(reverse_num)
 
-if reverse_num == temp:
-    print('palindrome')
-else:
-    print('not palindrome')
+# if reverse_num == temp:
+#     print('palindrome')
+# else:
+#     print('not palindrome')
 
-#print even digits in a number
+# #print even digits in a number
 
-num1 = int(input('enter the number: '))
+# num1 = int(input('enter the number: '))
+# sum = 0
+# while num1 > 0:
+#     digit = num1 % 10
+#     if digit % 2 == 0:
+#         print(digit)
+#     num1 = num1 // 10
+
+# # print prime digits in a number
+
+# num2 = int(input('enter the number: '))
+# list1 = [2, 3, 5, 7]
+# while num2 > 0:
+#     digit = num2 % 10
+#     if digit in list1:
+#         print(digit)
+#     num2 = num2 // 10
+
+# checking the given number is perfect number or not
+num3 = int(input('enter the number:'))
 sum = 0
-while num1 > 0:
-    digit = num1 % 10
-    if digit % 2 == 0:
-        print(digit)
-    num1 = num1 // 10
+for i in range(1, num3):
+    if num3 % i == 0:
+        sum +=i
 
-# print prime digits in a number
-
-num2 = int(input('enter the number: '))
-list1 = [2, 3, 5, 7]
-while num2 > 0:
-    digit = num2 % 10
-    if digit in list1:
-        print(digit)
-    num2 = num2 // 10
-
-
+if sum == num3:
+    print('perfect number')
+else:
+    print('not a perfect number')
