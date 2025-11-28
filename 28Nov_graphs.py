d4f8aec0b2101616ba0a283e5c454e2acb5fde86
DUDEKULA ANWAR <danwar2805@gmail.com>
2025-11-28 23:07:10 +0530

    Add files via upload

---
+++ 28Nov_graphs.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [10, 14, 12, 18, 20]
+
+plt.plot(x,y)
+
+x_scatter = [2, 4, 6, 8, 10]
+y_scatter = [5, 15, 8, 20, 14]
+plt.bar(x_scatter, y_scatter)
+
+plt.show()
+
+
